8d2275aad32db4d5cc75639a4bdda42074dc023d
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-25 13:15:53 -0500

    define records route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.get('/records', (req, res) => {
+  res.send("You're a wizard, Harry!")
+})
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
