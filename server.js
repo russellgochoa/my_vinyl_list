b6a5556d1f47be5a4dee4065737bb725d4b9ae4c
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-25 15:49:14 -0500

    add 'dev':'nodemon server.js' to scripts in package.json. run npm dev. create 'records' route

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
 
-app.get('/', (req, res) => {
-  res.send('This is root!')
+const { Record } = require('./models')
+
+app.get('/records', async (req, res) => {
+  const records = await Record.find({})
+  res.json(records)
 })
+
+// app.get('/', (req, res) => {
+//   res.send('This is root!')
+// })
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
